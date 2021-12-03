89d1f1aa480d5cb48a2eef5c04748cc1dd59c57a
gkrialashvili <giorgi.krialashvili08b@geolab.edu.ge>
2021-12-04 01:14:04 +0400

    last changes

--- WebCoiner/WebCoinerEntity.Designer.cs
+++ WebCoiner/WebCoinerEntity.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\userman\Source\Repos\WebCoiner\WebCoiner\WebCoinerEntity.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\Kriala\source\repos\5G-pro\WebCoiner\WebCoinerEntity.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
